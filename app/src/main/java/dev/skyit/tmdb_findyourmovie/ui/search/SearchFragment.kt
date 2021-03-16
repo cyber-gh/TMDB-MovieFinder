ffefc5ab1d6ae42ac86bd3332d11861e964388be
cristi <visanu.cristian@gmail.com>
2021-03-16 17:50:40 +0200

    added search fragment to bottom navigation

---
+++ app/src/main/java/dev/skyit/tmdb_findyourmovie/ui/search/SearchFragment.kt
@@ -0,0 +1,14 @@
+package dev.skyit.tmdb_findyourmovie.ui.search
+
+import android.os.Bundle
+import android.view.View
+import dagger.hilt.android.AndroidEntryPoint
+import dev.skyit.tmdb_findyourmovie.R
+import dev.skyit.tmdb_findyourmovie.generic.BaseFragment
+
+@AndroidEntryPoint
+class SearchFragment : BaseFragment(R.layout.fragment_search) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/dev/skyit/tmdb_findyourmovie/ui/search/SearchViewModel.kt
@@ -0,0 +1,11 @@
+package dev.skyit.tmdb_findyourmovie.ui.search
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.skyit.tmdb_findyourmovie.api.IMoviesAPIClient
+import dev.skyit.tmdb_findyourmovie.repo.UserRepo
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchViewModel@Inject constructor() : ViewModel() {
+}

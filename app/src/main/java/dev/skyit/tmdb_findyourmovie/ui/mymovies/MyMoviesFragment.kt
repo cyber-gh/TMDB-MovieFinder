d8c329f5e9a4dfe3526e77d37bc30e16d2b8ce70
cristi <visanu.cristian@gmail.com>
2021-03-16 14:42:21 +0200

    added list_item_movie_already_watched.xml, list_item_movie_watch_later.xml, updated watch later list

--- app/src/main/java/dev/skyit/tmdb_findyourmovie/ui/mymovies/MyMoviesFragment.kt
+++ app/src/main/java/dev/skyit/tmdb_findyourmovie/ui/mymovies/MyMoviesFragment.kt
@@ -1,10 +1,82 @@
 package dev.skyit.tmdb_findyourmovie.ui.mymovies
 
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.FragmentNavigatorExtras
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import by.kirich1409.viewbindingdelegate.viewBinding
+import coil.Coil
+import coil.load
+import coil.request.ImageRequest
 import dagger.hilt.android.AndroidEntryPoint
 import dev.skyit.tmdb_findyourmovie.R
+import dev.skyit.tmdb_findyourmovie.api.models.movielist.MovieMinimal
+import dev.skyit.tmdb_findyourmovie.databinding.FragmentMyMoviesBinding
+import dev.skyit.tmdb_findyourmovie.databinding.ListItemMovieAlreadyWatchedBinding
+import dev.skyit.tmdb_findyourmovie.databinding.ListItemMovieWatchLaterBinding
+import dev.skyit.tmdb_findyourmovie.databinding.ListItemRecentlyWatchedBinding
 import dev.skyit.tmdb_findyourmovie.generic.BaseFragment
+import dev.skyit.tmdb_findyourmovie.ui.profile.ProfileFragmentDirections
+import dev.skyit.tmdb_findyourmovie.ui.utils.SimpleRecyclerAdapter
 
 @AndroidEntryPoint
 class MyMoviesFragment : BaseFragment(R.layout.fragment_my_movies) {
 
+    private val vModel: MyMoviesViewModel by viewModels()
+    private val binding: FragmentMyMoviesBinding by viewBinding()
+
+    private lateinit var watchLaterAdapter: SimpleRecyclerAdapter<MovieMinimal, ListItemMovieWatchLaterBinding>
+    private lateinit var alreadyWatchedAdapter: SimpleRecyclerAdapter<MovieMinimal, ListItemMovieAlreadyWatchedBinding>
+
+    private fun buildWatchLaterList() {
+        watchLaterAdapter = SimpleRecyclerAdapter({
+            ListItemMovieWatchLaterBinding.inflate(it)
+        }, { data ->
+            this.moviePreview.transitionName = data.id.toString()
+            this.moviePreview.load(data.posterPath)
+            this.moviePreviewName.text = data.title
+
+            Coil.enqueue(ImageRequest.Builder(requireContext())
+                    .data(data.backdropPath)
+                    .build())
+        }, onItemClick = { v, item ->
+            findNavController().navigate(ProfileFragmentDirections
+                    .actionNavigationProfileToMovieDetailsFragment(
+                            item, v.moviePreview.transitionName, item.id
+                    ),
+                    FragmentNavigatorExtras(v.moviePreview to v.moviePreview.transitionName)
+            )
+        })
+
+        binding.watchLaterList.adapter = watchLaterAdapter
+        binding.watchLaterList.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+
+        vModel.moviesList.observe(viewLifecycleOwner, {
+            watchLaterAdapter.updateData(ArrayList(it))
+        })
+
+        vModel.loadData()
+    }
+
+    private fun buildAlreadyWatchedList() {
+
+    }
+
+    private fun bindUI() {
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        bindUI()
+
+        buildWatchLaterList()
+
+        buildAlreadyWatchedList()
+    }
 }
--- app/src/main/java/dev/skyit/tmdb_findyourmovie/ui/mymovies/MyMoviesViewModel.kt
+++ app/src/main/java/dev/skyit/tmdb_findyourmovie/ui/mymovies/MyMoviesViewModel.kt
@@ -3,11 +3,31 @@ package dev.skyit.tmdb_findyourmovie.ui.mymovies
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.skyit.tmdb_findyourmovie.api.IMoviesAPIClient
+import dev.skyit.tmdb_findyourmovie.api.models.movielist.MovieMinimal
 import dev.skyit.tmdb_findyourmovie.generic.BaseViewModel
+import dev.skyit.tmdb_findyourmovie.repo.UserRepo
+import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
-class MyMoviesViewModel @Inject constructor() : BaseViewModel() {
+class MyMoviesViewModel @Inject constructor(
+        private val api: IMoviesAPIClient,
+        private val userRepo: UserRepo
+) : BaseViewModel() {
+    fun loadData() {
+        viewModelScope.launch {
 
+            val movies = api.getTrendingMovies()
+            moviesList.postValue(movies)
+
+            Timber.i("Movies are")
+            Timber.i(movies.toString())
+        }
+    }
+
+    val moviesList: MutableLiveData<List<MovieMinimal>> = MutableLiveData()
 }
